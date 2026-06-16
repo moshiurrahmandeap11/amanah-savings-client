a15a4eee0db8f7cff7ec48f5a999cbef4f7cf3b7
sanzid-shojib <portfolio.2872@gmail.com>
2026-06-17 02:15:27 +0600

    fix

--- src/app/components/shared/AxiosInstance/AxiosInstance.jsx
+++ src/app/components/shared/AxiosInstance/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create instance
 const axiosInstance = axios.create({
-  baseURL: "https:api.sanchoybondhu.com/api", 
+  baseURL: "http:api.sanchoybondhu.com/api", 
   withCredentials: true, 
 });
 
